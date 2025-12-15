5a7f19b878a1520c7df34a9e00f83e9fb93fef53
Jivansatapathy <satapathyjjivan@gmail.com>
2025-12-15 23:05:46 +0530

    Chnages frontend

--- script.js
+++ script.js
@@ -117,8 +117,7 @@ async function fetchRazorpayKey() {
     }
 
     // fallback
-    //RAZORPAY_KEY_ID = "rzp_live_Ri3wbsv2HCMv4v";
-    RAZORPAY_KEY_ID = "rzp_test_Ri4TRiPWd8jb0Q";
+    RAZORPAY_KEY_ID = "rzp_live_Ri3wbsv2HCMv4v";
 }
 
 fetchRazorpayKey();
